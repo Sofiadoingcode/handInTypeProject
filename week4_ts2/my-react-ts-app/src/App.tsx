6af005dd01a616e7504da0ac6f7e41eee662e04e
Sofiadoingcode <88271469+Sofiadoingcode@users.noreply.github.com>
2023-02-22 11:42:35 +0100

    Exercise Part 1

---
+++ week4_ts2/my-react-ts-app/src/App.tsx
@@ -0,0 +1,100 @@
+import { ReactElement, useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [name, setName] = useState("")
+
+  return (
+    <div className="App">
+      <InputComp setName={setName}/>
+      <OutPutComp name={name}/>
+      <PeopleViewer/>
+      <AddPerson/>
+    </div>
+    
+  )
+}
+
+function AddPerson(): ReactElement {
+  
+
+
+
+}
+
+
+
+function InputComp(props: {setName: React.Dispatch<React.SetStateAction<string>>}): ReactElement {
+
+  return (
+  <div>
+    <input placeholder='write name' onChange={evt => props.setName(evt.target.value)} className='input'></input>
+  </div>
+  )
+
+}
+
+function OutPutComp(props: {name: string}): ReactElement {
+  return(
+    <div className='textName'>
+        {props.name}
+      </div>
+
+  )
+
+}
+
+function PeopleViewer(): ReactElement {
+  const [people, setPeople] = useState([]);
+
+  type Person = {
+    name: string, 
+    age: number,
+    city: string
+
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:3008/person")
+    .then((res) => res.json())
+    .then((res) => setPeople(res))
+
+  }, []
+  );
+
+
+  return(
+    <div>
+      <table>
+        <thead>
+          <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th>City</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {people.map(
+            (person: Person) => 
+            <tr>
+              <td>{person.name}</td>
+              <td>{person.age}</td>
+              <td>{person.city}</td>
+
+
+            </tr>
+            
+          )}
+
+        </tbody>
+      </table>
+
+    </div>
+
+  )
+}
+
+
+export default App
